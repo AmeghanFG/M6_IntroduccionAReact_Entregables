8a6e88345f4c96a4cec44b2b0ce2c8b87a1469ed
Meghan <afigueroa57@ucol.mx>
2025-07-26 15:46:45 -0600

    Iniciando nuevo proyecto y primeros pasos

---
+++ M6-4_Ciclo-de-vida-de-Componentes/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import './App.css'
+
+function App() {
+  // Estados
+  const [distancia, setDistancia] = useState(0);
+  const [combustible, setCombustible] = useState(100);
+  const [estadoNave, setEstadoNave] = useState('En órbita');
+  const [planetasVisitados, setPlanetasVisitados] = useState([]);
+
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+export default App;
