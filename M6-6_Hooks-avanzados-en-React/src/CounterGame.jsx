23445742eed338ab50c5d4be27dfd01bc0c2e06d
Meghan <afigueroa57@ucol.mx>
2025-07-29 22:32:40 -0600

    Ordenando el ccódigo

--- M6-6_Hooks-avanzados-en-React/src/CounterGame.jsx
+++ M6-6_Hooks-avanzados-en-React/src/CounterGame.jsx
@@ -1,31 +1,34 @@
 import { useReducer, useRef, useCallback, useEffect } from 'react'
 
+const initialState = { count: 0, history: [] }
+
+function reducer (state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {
+        count: state.count + 1, history: [...state.history, `+1 (Nuevo valor: ${state.count + 1})`]
+      }
+    case 'decrement':
+      return {
+        count: state.count - 1, history: [...state.history, `-1 (Nuevo valor: ${state.count - 1})`]
+      }
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
 const CounterGame = () => {
   const incrementBtnRef = useRef(null)
-  const initialState = { count: 0, history: [] }
 
   const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    incrementBtnRef.current.focus()
-  }, [])
-
-  function reducer (state, action) {
-    switch (action.type) {
-      case 'increment':
-        return {
-          count: state.count + 1, history: [...state.history, `+1 (Nuevo valor: ${state.count + 1})`]
-        }
-      case 'decrement':
-        return {
-          count: state.count - 1, history: [...state.history, `-1 (Nuevo valor: ${state.count - 1})`]
-        }
-      case 'reset':
-        return initialState
-      default:
-        return state
+    if (incrementBtnRef.current) {
+      incrementBtnRef.current.focus()
     }
-  }
+  }, [])
 
   const handleIncrement = useCallback(() => {
     dispatch({ type: 'increment' })
@@ -35,13 +38,16 @@ const CounterGame = () => {
     dispatch({ type: 'decrement' })
   }, [])
 
+  const handleReset = useCallback(() => {
+    dispatch({ type: 'reset' })
+  }, [])
+
   return (
     <div>
       <h2>Contador: {state.count}</h2>
-      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
-
       <button ref={incrementBtnRef} onClick={handleIncrement}>+</button>
       <button onClick={handleDecrement}>-</button>
+      <button onClick={handleReset}>Reset</button>
 
       <h3>Historial de cambios:</h3>
       <ul>
