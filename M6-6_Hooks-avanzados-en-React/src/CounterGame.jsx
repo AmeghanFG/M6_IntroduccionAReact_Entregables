dc929da8a37c4dfb1772eb04b9f92c736266ad11
Meghan <afigueroa57@ucol.mx>
2025-07-29 23:03:48 -0600

    Agregando comentarios de como funciona el código

--- M6-6_Hooks-avanzados-en-React/src/CounterGame.jsx
+++ M6-6_Hooks-avanzados-en-React/src/CounterGame.jsx
@@ -1,35 +1,37 @@
 import { useReducer, useRef, useCallback, useEffect } from 'react'
 
-const initialState = { count: 0, history: [] }
+const initialState = { count: 0, history: [] } /* Estado inicial del componente, count guarda el valor actual del contador y history guarda en forma de arreglo la secuencia de cambios realizados */
 
-function reducer (state, action) {
-  switch (action.type) {
+function reducer (state, action) { // state es el estado actual antes de la acción, action es un objeto con al menos una propiedad type, que indica qué tipo de acción se debe ejecutar.
+  switch (action.type) { // Si se activa increment se agrega uno a state
     case 'increment':
       return {
         count: state.count + 1, history: [...state.history, `+1 (Nuevo valor: ${state.count + 1})`]
-      }
+      } // En el count se suma/resta y se agrega la acción en history con spread operator para no perder las acciones anteriores
     case 'decrement':
       return {
         count: state.count - 1, history: [...state.history, `-1 (Nuevo valor: ${state.count - 1})`]
       }
     case 'reset':
-      return initialState
+      return initialState // Regresar al estado inicial
     default:
       return state
   }
 }
 
 const CounterGame = () => {
-  const incrementBtnRef = useRef(null)
+  const incrementBtnRef = useRef(null) // Crea referencia  mutable que persiste entre renders. Al inicializar con null, se usará para referenciar directamente el DOM del botón "+".
 
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState) // state es el estado actual, que inicialmente es initialState y dispatch es una función para mandar acciones al reducer y actualizar el estado
 
   useEffect(() => {
-    if (incrementBtnRef.current) {
-      incrementBtnRef.current.focus()
+    if (incrementBtnRef.current) { // Se hace referencia al boton +
+      incrementBtnRef.current.focus() // Se agrega el .focus() para que el btn tenga el foco en automatico al recargar la página
+      // TODO: Investigar para que se usa el .current
     }
-  }, [])
+  }, []) // Solo se ejecuta una vez
 
+  // Acciones
   const handleIncrement = useCallback(() => {
     dispatch({ type: 'increment' })
   }, [])
@@ -42,7 +44,7 @@ const CounterGame = () => {
     dispatch({ type: 'reset' })
   }, [])
 
-  return (
+  return ( // Interfaz
     <div>
       <h2>Contador: {state.count}</h2>
       <button ref={incrementBtnRef} onClick={handleIncrement}>+</button>
