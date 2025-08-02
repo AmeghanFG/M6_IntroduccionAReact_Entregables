8bca6d2fb6fb8f1db4e883e6fc48f429ea19b833
Meghan <afigueroa57@ucol.mx>
2025-08-02 02:22:20 -0600

    Estilos para página 404

--- M6-7_Manejo-de-Rutas/src/pages/Home.jsx
+++ M6-7_Manejo-de-Rutas/src/pages/Home.jsx
@@ -5,6 +5,8 @@ const Home = () => {
     <div className='page-home'>
       <h1>Home</h1>
       <h2>Bienvenidos al sistema de citas del hospital Mirari</h2>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident officia quas, rerum accusamus aliquam totam aut eveniet perspiciatis qui magnam quis! Nulla itaque sit temporibus vitae consectetur aspernatur accusamus, expedita impedit, corrupti, vel necessitatibus beatae laudantium facere maiores amet minima possimus dignissimos porro eaque accusantium explicabo in. Recusandae possimus natus deleniti vitae praesentium voluptatem ex qui quis earum aut nostrum quam aspernatur magni omnis, dolores tenetur cum id! Minima ipsa eius consequatur vero nisi est alias eos nam quis, aliquam autem molestias voluptatem id amet dolore ducimus. Obcaecati dolorum adipisci nobis rerum ab. Repellat sapiente perspiciatis corrupti excepturi doloribus molestias?</p>
     </div>
   )
 }
