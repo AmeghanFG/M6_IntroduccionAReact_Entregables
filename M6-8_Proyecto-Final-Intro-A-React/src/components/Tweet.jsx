2acc5d45161ac1966c6df10c07957eeca6c6ce6b
Meghan <afigueroa57@ucol.mx>
2025-08-03 20:17:49 -0600

    Agregando el código necesario para Tweets en components

---
+++ M6-8_Proyecto-Final-Intro-A-React/src/components/Tweet.jsx
@@ -0,0 +1,12 @@
+const Tweet = ({ tweet, onLike }) => {
+  return (
+    <div className='tweet'>
+      <p>{tweet.text}</p>
+      <button onClick={() => onLike(tweet.id)}>
+        ❤️ {tweet.likes}
+      </button>
+    </div>
+  )
+}
+
+export default Tweet
